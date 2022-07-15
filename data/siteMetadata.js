2b65d2385a7b48421691e1b1a254decc5d53eee2
Halip26 <halipuddin.angko@gmail.com>
2022-07-15 19:45:22 +0800

    second commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Halip Blog',
+  author: 'Halip26',
+  headerTitle: 'Halip Blog',
+  description: 'welcome to my blog, my thoughts are written here',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
